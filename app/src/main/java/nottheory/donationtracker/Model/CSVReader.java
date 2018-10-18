322682cb9977145d093d71c2eed76b6780b55ecf
varunsl <varunsl@hotmail.com>
2018-10-17 20:30:47 -0400

    Update CSVReader.java

--- app/src/main/java/nottheory/donationtracker/Model/CSVReader.java
+++ app/src/main/java/nottheory/donationtracker/Model/CSVReader.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
