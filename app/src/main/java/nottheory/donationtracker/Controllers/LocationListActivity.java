83a4f312c174115e7c62b500437e534e5207e7d8
Tyler Brazelton <tyler.brzltn.99@gmail.com>
2018-10-17 23:48:55 -0400

    updated location list stuff

--- app/src/main/java/nottheory/donationtracker/Controllers/LocationListActivity.java
+++ app/src/main/java/nottheory/donationtracker/Controllers/LocationListActivity.java
@@ -5,28 +5,80 @@ import java.io.IOException;
 import java.io.InputStream;
 import nottheory.donationtracker.R;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View;
+import android.view.LayoutInflater;
+
 import nottheory.donationtracker.R;
 
 public class LocationListActivity extends AppCompatActivity {
+
+    private RecyclerView locationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_list);
+        try{
+            CSVReader reader = new CSVReader(getResources().openRawResource(R.raw.locationdata));
+            locationList = findViewById(R.id.locationList);
+            String[] locationArray = new String[reader.size() - 1];
+            for (int i = 1; i < reader.size() + 1; i++) {
+                locationArray[i - 1] = reader.getData(i, 1);
+            }
+            locationList.setAdapter(new LocationAdapter(this, locationArray));
+        } catch(IOException e) {
+            System.out.println("IOException, csv file cannot be read");
+            return;
+        }
     }
 
-    protected void pushData() {
-        InputStream inputStream = getResources().openRawResource(R.raw.locationdata);
-        try {
-            CSVReader acsvreader = new CSVReader(inputStream);
-            while () {
+    private class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.LocationViewHolder> {
+        private String[] locations;
+        private Context context;
+        public class LocationViewHolder extends RecyclerView.ViewHolder {
+            private Button location;
 
+            public LocationViewHolder(Button location) {
+                super(location);
+                this.location = location;
             }
-        } catch(IOException e){
-            System.out.println("IOException. .csv file could not be read");
         }
 
+        public LocationAdapter(Context context, String[] locations) {
+            this.locations = locations;
+            this.context = context;
+        }
+
+        public LocationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Button button =(Button) LayoutInflater.from(context).inflate(R.layout.location_recyclerview_row, parent, false);
+            return new LocationViewHolder(button);
+        }
+
+        public void onBindViewHolder(LocationViewHolder viewHolder, int position) {
+            viewHolder.location.setText(locations[position]);
+        }
+
+        public int getItemCount() {
+            return locations.length;
+        }
     }
+
+//    protected void pushData() {
+//        InputStream inputStream = getResources().openRawResource(R.raw.locationdata);
+//        try {
+//            CSVReader acsvreader = new CSVReader(inputStream);
+//            while () {
+//
+//            }
+//        } catch(IOException e){
+//            System.out.println("IOException. .csv file could not be read");
+//        }
+//
+//    }
 }
