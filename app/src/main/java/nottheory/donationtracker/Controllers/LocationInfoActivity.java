3e73b33c6d0b27c6e9e2c92ba39751d33df17803
joey <josephcrawford99@gmail.com>
2018-10-17 21:25:49 -0400

    Stub out CSVReader cols method and implement the ability to read in info about a location

--- app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
+++ app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
@@ -7,18 +7,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import nottheory.donationtracker.Model.CSVReader;
 
 import nottheory.donationtracker.R;
 
 public class LocationInfoActivity extends AppCompatActivity {
 
+    private TextView locationText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_info);
+        locationText = findViewById(R.id.locationInfoDisplay);
+        CSVReader reader = new CSVReader(getResources().openRawResource(R.raw.LocationData.csv));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        String text = "";//append to txt
+        for(int i = 1; i < reader.size() + 1; i++) { //iterate rows
+            for(int j = 1; j < reader.cols() + 1; j++) { //iterate cols
+                text += reader.getData(i,j);
+                text += " ";
+            }
+            text += "\n";
+        }
 
+        locationText.setText(text);
+        locationText.setVisibility(View.VISIBLE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +43,7 @@ public class LocationInfoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
 }
