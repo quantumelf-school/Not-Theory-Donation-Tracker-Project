c55f81b17c527a123c27bdfe7cc55c636563a9b2
joey <josephcrawford99@gmail.com>
2018-10-17 20:28:29 -0400

    Add package to CSVReader header

--- app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
+++ app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
@@ -1,11 +1,14 @@
-package nottheory.donationtracker;
+package nottheory.donationtracker.Controllers;
 
+import java.io.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import nottheory.donationtracker.R;
 
 public class LocationInfoActivity extends AppCompatActivity {
 
