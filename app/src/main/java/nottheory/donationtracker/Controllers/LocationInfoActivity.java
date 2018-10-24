d91a5e4660342def74250a4774386efe4dccc48a
joey <josephcrawford99@gmail.com>
2018-10-24 13:25:46 -0400

    Create DonationCollection and implement in the Location and LocationCollection model classes

--- app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
+++ app/src/main/java/nottheory/donationtracker/Controllers/LocationInfoActivity.java
@@ -44,14 +44,6 @@ public class LocationInfoActivity extends AppCompatActivity {
         } catch(IOException e) {
             System.out.println("IOException. The .csv file could not be read");
         }
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
     }
 
