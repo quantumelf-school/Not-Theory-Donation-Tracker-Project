3736324fb364e8e228bc645d702f7d85263439f6
joey <josephcrawford99@gmail.com>
2018-11-05 08:51:10 -0500

    Add markers to google map

--- app/src/main/java/nottheory/donationtracker/Controllers/MapsActivity.java
+++ app/src/main/java/nottheory/donationtracker/Controllers/MapsActivity.java
@@ -8,8 +8,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import nottheory.donationtracker.Model.Location;
+import nottheory.donationtracker.Model.LoginManager;
 import nottheory.donationtracker.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -41,8 +44,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        for(Location l: LoginManager.locations.getLocations()) {
+            mMap.addMarker(new MarkerOptions().position(new LatLng(l.getLat(), l.getLong())).title(l.toString()));
+        }
+        
+
     }
 }
