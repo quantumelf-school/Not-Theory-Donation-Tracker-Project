a94b1dd6cd2e4b348e82671209e51ef58c1bedd2
joey <josephcrawford99@gmail.com>
2018-11-15 13:49:05 -0500

    Fix JoeyUnitTest Linter issues

--- app/src/test/java/nottheory/donationtracker/JoeyUnitTest.java
+++ app/src/test/java/nottheory/donationtracker/JoeyUnitTest.java
@@ -20,21 +20,38 @@ import static junit.framework.TestCase.fail;
 public class JoeyUnitTest {
     private DonationCollection testCollection;
 
+    /**
+     * Initializes a DonationCollection to test
+     */
     @Before
     public void setUp(){
-        testCollection = new DonationCollection();
-        testCollection.addDonation(new Donation("TEST", "A","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "B","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "C","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "D","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "E","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "F","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "g","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "H","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "I","TEST","TEST","TEST", "TEST"));
-        testCollection.addDonation(new Donation("TEST", "J","TEST","TEST","TEST", "TEST"));
+        testCollection = new DonationCollection(); //Feature envy is most concise way to add these
+        //elements to the collection for testing purposes
+        testCollection.addDonation(new Donation("TEST", "A",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "B",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "C",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "D",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "E",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "F",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "g",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "H",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "I",
+                "TEST","TEST","TEST", "TEST"));
+        testCollection.addDonation(new Donation("TEST", "J",
+                "TEST","TEST","TEST", "TEST"));
     }
 
+    /**
+     * Tests that an element can be found by its name.
+     */
     @Test
     public void testFindElementInCollection() {
         String text = "";
@@ -47,7 +64,10 @@ public class JoeyUnitTest {
         assertEquals("Element Not correctly found in DonationCollection", text,
                 testCollection.getDonationByName("D").toString());
     }
-
+    /**
+     * Tests that an element can be found by its name if the case does not match with the query.
+     * This achieves total branch coverage beside the exception
+     */
     @Test
     public void testFindElementInCollectionUnmatchedCase() {
         String text = "";
@@ -57,9 +77,13 @@ public class JoeyUnitTest {
         text += "Value: " + "TEST" + "\n";
         text += "Category: " + "TEST" + "\n";
         text += "Comments (Optional): " + "TEST" + "\n";
-        assertEquals("Uppercase element not correctly found in DonationCollection when searching with lowercase name", text,
+        assertEquals("Uppercase element not correctly found " +
+                        "in DonationCollection when searching with lowercase name", text,
                 testCollection.getDonationByName("b").toString());
     }
+    /**
+     * Tests that an element can be found by its name if both are lowercase.
+     */
     @Test
     public void testFindElementInCollectionLowercase() {
         String text = "";
@@ -72,6 +96,9 @@ public class JoeyUnitTest {
         assertEquals("Lowercase element not correctly found in DonationCollection", text,
                 testCollection.getDonationByName("g").toString());
     }
+    /**
+     * Tests that an element can be found by its name if you search in Uppercase.
+     */
     @Test
     public void testFindElementInCollectionUnmatchedCaseLowercase() {
         String text = "";
@@ -81,14 +108,20 @@ public class JoeyUnitTest {
         text += "Value: " + "TEST" + "\n";
         text += "Category: " + "TEST" + "\n";
         text += "Comments (Optional): " + "TEST" + "\n";
-        assertEquals("Lowercase element not correctly found in DonationCollection when searching with uppercase name", text,
+        assertEquals("Lowercase element not correctly found in DonationCollection " +
+                        "when searching with uppercase name", text,
                 testCollection.getDonationByName("G").toString());
     }
+    /**
+     * Tests that a name queried that is not in the collection will throw NoSuchElementException
+     * This achieves total coverage as the for loop has been run and the exception has been tested
+     */
     @Test
     public void testThrowNoSuchElement() {
         try {
             testCollection.getDonationByName("K");
-            fail("NoSuchElementException not thrown when a donation by a name not in the collection is passed in");
+            fail("NoSuchElementException not thrown when a donation by a name not " +
+                    "in the collection is passed in");
         } catch (NoSuchElementException e) {
             //Test Passed
         }
